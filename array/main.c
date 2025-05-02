be17464c3599a5c6e49c7ce0fc0633f83a5e6fd5
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-05-03 00:40:53 +0300

    Initial commit

--- array/main.c
+++ array/main.c
@@ -1,14 +1,43 @@
-//
-//  main.c
-//  array
-//
-//  Created by Gayrat Rahmatulayev on 2.05.2025.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Array {
+    int A[20];
+    int size;
+    int length;
+};
+
+
+void Display(struct Array array){
+    for(int i = 0; i < array.length; i++){
+        printf("%d ", array.A[i]);
+    }
+}
+
+
+void Append(struct Array *array, int x){
+    if (array->length < array->size) {
+        array->A[array->length++] = x;
+    }
+}
+
+
+void Insert(struct Array *array, int index, int x){
+    for (int i = array->length; i > index; i--) {
+        array->A[i] = array->A[i-1];
+    }
+    array->A[index] = x;
+    array->length++;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    struct Array my_array = {{2, 4, 1, 5, 2}, 10, 5};
+    
+    Append(&my_array, 8);
+    Insert(&my_array, 2, 5);
+    Display(my_array);
+    printf("\n");
+    
     return 0;
 }
