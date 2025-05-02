865b855089723aa7d9c71c92bf4d16b02af45de5
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-05-02 23:34:17 +0300

    Initial Commit

---
+++ array/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  array
+//
+//  Created by Gayrat Rahmatulayev on 2.05.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
