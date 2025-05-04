d449e34c3c0d024b533b66c70d10e6c2332de7bb
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-05-04 03:09:46 +0300

    added functioons Delete and LinearSearch

--- array/main.c
+++ array/main.c
@@ -33,11 +33,34 @@ void Insert(struct Array *array, int index, int x){
 }
 
 
+int Delete(struct Array *array, int index){
+    if (index >= 0 && array->length < array->size && index <= array->length) {
+        int x = array->A[index];
+        for (int i = index; i < array->length - 1; i++) {
+            array->A[i] = array->A[i+1];
+        }
+        array->length--;
+        return x;
+    }
+    return 0;
+}
+
+
+int LinearSearch(struct Array *array, int key){
+    for (int i = 0; i < array->length; i++){
+        if (key == array->A[i]){
+            printf("%d Found at index %i\n",key, i);
+            return i;
+        }
+    }
+    return -1;
+}
+
+
 int main(int argc, const char * argv[]) {
-    struct Array my_array = {{2, 4, 1, 5, 2}, 10, 5};
+    struct Array my_array = {{1, 2, 3, 4, 5}, 10, 5};
     
-    Append(&my_array, 8);
-    Insert(&my_array, 2, 5);
+    LinearSearch(&my_array, 3);
     Display(my_array);
     printf("\n");
     
