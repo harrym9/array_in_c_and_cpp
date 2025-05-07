49b9e97bd4876db0da647cf4677f9f5791124781
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-05-07 07:39:24 +0300

    Fixed function calls: replaced call-by-address with call-by-value where necessary

--- array/main.c
+++ array/main.c
@@ -46,9 +46,9 @@ int Delete(struct Array *array, int index){
 }
 
 
-int LinearSearch(struct Array *array, int key){
-    for (int i = 0; i < array->length; i++){
-        if (key == array->A[i]){
+int LinearSearch(struct Array array, int key){
+    for (int i = 0; i < array.length; i++){
+        if (key == array.A[i]){
             printf("%d Found at index %i\n",key, i);
             return i;
         }
@@ -57,16 +57,16 @@ int LinearSearch(struct Array *array, int key){
 }
 
 
-int BinarySearch(struct Array *array, int key){
+int BinarySearch(struct Array array, int key){
     int low = 0;
-    int high = array->length - 1;
+    int high = array.length - 1;
     
     while (low <= high){
         int mid = (low + high) / 2;
-        if (array->A[mid] == key){
+        if (array.A[mid] == key){
             return mid;
         }
-        else if (key < array->A[mid]){
+        else if (key < array.A[mid]){
             high = mid - 1;
         }
         else {
@@ -77,15 +77,15 @@ int BinarySearch(struct Array *array, int key){
 }
 
 
-int RBinarySearch(struct Array *array, int low, int high, int key){
+int RBinarySearch(struct Array array, int low, int high, int key){
     if (low > high) {
         return -1;
     }
     int mid = (low + high) / 2;
-    if (array->A[mid] == key){
+    if (array.A[mid] == key){
         return mid;
     }
-    else if (key > array->A[mid]){
+    else if (key > array.A[mid]){
         return RBinarySearch(array, mid + 1, high, key);
     }
     else{
@@ -95,10 +95,10 @@ int RBinarySearch(struct Array *array, int low, int high, int key){
 }
 
 
-int Get(struct Array *array, int index){
-    if (index < 0 && index >= array->length)
+int Get(struct Array array, int index){
+    if (index < 0 && index >= array.length)
         return -1;
-    return array->A[index];
+    return array.A[index];
 }
 
 
@@ -112,41 +112,45 @@ void Set(struct Array *array, int index, int new_key){
 }
 
 
-int Max(struct Array *array){
-    int max = array->A[0];
-    for (int i = 1; i < array->length; i++){
-        if (array->A[i] > max) {
-            max = array->A[i];
+int Max(struct Array array){
+    int max = array.A[0];
+    for (int i = 1; i < array.length; i++){
+        if (array.A[i] > max) {
+            max = array.A[i];
         }
     }
     return max;
 }
 
 
-int Min(struct Array *array){
-    int min = array->A[0];
-    for (int i = 1; i < array->length; i++){
-        if (array->A[i] < min) {
-            min = array->A[i];
+int Min(struct Array array){
+    int min = array.A[0];
+    for (int i = 1; i < array.length; i++){
+        if (array.A[i] < min) {
+            min = array.A[i];
         }
     }
     return min;
 }
 
 
-double Average(struct Array *array){
-    int total = 0;
-    for (int i = 0; i < array->length; i++){
-        total += array->A[i];
-    }
-    return total / array->length;
+int Sum(struct Array array) {
+    int sum = 0;
+    for (int i = 0; i < array.length; i++)
+        sum += array.A[i];
+    return sum;
+}
+
+
+float Average(struct Array array){
+    return (float) Sum(array) / array.length;
 }
 
 
 int main(int argc, const char * argv[]) {
     struct Array my_array = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 10};
     
-    printf("%f\n", Average(&my_array));
+    printf("%.2f\n", Average(my_array));
     Display(my_array);
     printf("\n");
     
